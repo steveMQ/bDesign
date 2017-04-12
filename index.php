a5b8f818f04a30628873b67c324fa774cbb081c9
steveMQ <stevequatrale@gmail.com>
2017-04-12 17:53:04 -0400

    'initial commit!'

---
+++ index.php
@@ -0,0 +1,16 @@
+<h1>hello world!</h1>
+
+<h3>this shit seems to be working. cool.</h3>
+
+<h5>I changed something...is live reload working?</h5>
+
+<h3>hmmm....</h3>
+
+<span>this shit is pretty cool</span>
+
+
+
+<script>
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>')
+</script>
