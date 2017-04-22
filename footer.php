4c44686847142f43ae91aefa614e4f310d6e6f11
steveMQ <stevequatrale@gmail.com>
2017-04-21 22:21:29 -0400

    trying to implement social icons. will fix next dev session.

--- footer.php
+++ footer.php
@@ -3,6 +3,9 @@
         <div id="ribbonRow">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/contactRibbon.png" id="contactRibbon"/>
         </div>
+        <div id="contactBlurb">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam faucibus lectus id mi porta finibus. Ut rutrum commodo eros malesuada interdum. Nullam ut imperdiet.</span>
+        </div>
         <div id="contactForm">
           <form>
             <div class="form-group">
@@ -19,6 +22,21 @@
             </div>
             <button type="submit" class="btn btn-default">Submit</button>
           </form>
+        </div>
+        <div id="socialIcons">
+          <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/socialSprite.png" />
+          </a>
+          <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/socialSprite.png" />
+          </a>
+          <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/socialSprite.png" />
+          </a>
+          <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/socialSprite.png" />
+          </a>
+
         </div>
       </div>
       <img src="<?php echo get_template_directory_uri(); ?>/assets/imgs/woodBot.png" id="woodBot"/>
