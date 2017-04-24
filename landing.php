2f5af4f64c33a3925ef0f469a81e06eab18edddd
steveMQ <stevequatrale@gmail.com>
2017-04-24 19:22:57 -0400

    turning off carousel controls until I can fix the glyphicon issue

--- landing.php
+++ landing.php
@@ -54,14 +54,14 @@
       </div>
 
       <!-- controls -->
-      <a class="left carousel-control" href="#bpCarousel" role="button" data-slide="prev">
+      <!-- <a class="left carousel-control" href="#bpCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#bpCarousel" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-      </a>
+      </a> -->
 
     </div>
   </div>
